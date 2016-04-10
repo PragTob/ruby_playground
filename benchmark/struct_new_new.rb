d799fe4a7e53c385ba161a4aa99880f1f8d76a8e
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-04-10 21:20:08 +0200

    Include accessing the values + Array and Hash

--- benchmark/struct_new_new.rb
+++ benchmark/struct_new_new.rb
@@ -2,24 +2,48 @@ require 'benchmark/ips'
 
 Benchmark.ips do |bm|
   bm.report "Struct.new(...).new" do
-    Struct.new(:start, :end).new(10, 20)
+    value = Struct.new(:start, :end).new(10, 20)
+    value.start
+    value.end
   end
 
   SavedStruct = Struct.new(:start, :end)
   bm.report "SavedStruct.new" do
-    SavedStruct.new(10, 20)
+    value = SavedStruct.new(10, 20)
+    value.start
+    value.end
+  end
+
+  bm.report "2 element array" do
+    value = [10, 20]
+    value.first
+    value.last
+  end
+
+  bm.report "Hash with 2 keys" do
+    value = {start: 10, end: 20}
+    value[:start]
+    value[:end]
   end
 
   bm.compare!
 end
+
 # Ruby 2.3:
 # Calculating -------------------------------------
-# Struct.new(...).new    12.074k i/100ms
-# SavedStruct.new   155.934k i/100ms
+# Struct.new(...).new    11.739k i/100ms
+# SavedStruct.new   142.314k i/100ms
+# 2 element array   168.250k i/100ms
+# Hash with 2 keys   108.454k i/100ms
 # -------------------------------------------------
-# Struct.new(...).new    125.275k (± 4.7%) i/s -    627.848k
-# SavedStruct.new      4.798M (± 1.5%) i/s -     24.014M
+# Struct.new(...).new    111.902k (± 9.3%) i/s -    563.472k
+# SavedStruct.new      3.930M (± 5.7%) i/s -     19.639M
+# 2 element array      6.911M (± 4.2%) i/s -     34.491M
+# Hash with 2 keys      2.418M (± 3.8%) i/s -     12.147M
 #
 # Comparison:
-#   SavedStruct.new:  4797720.9 i/s
-# Struct.new(...).new:   125274.7 i/s - 38.30x slower
+#   2 element array:  6911144.9 i/s
+# SavedStruct.new:  3929604.4 i/s - 1.76x slower
+# Hash with 2 keys:  2417552.9 i/s - 2.86x slower
+# Struct.new(...).new:   111901.9 i/s - 61.76x slower
+

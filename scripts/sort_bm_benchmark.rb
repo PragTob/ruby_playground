86ef92e95a3b3c07af7bfc7b27f02833f3dc011e
Tobias Pfeiffer <pragtob@gmail.com>
2017-04-18 14:10:01 +0200

    block correction

--- scripts/sort_bm_benchmark.rb
+++ scripts/sort_bm_benchmark.rb
@@ -1,11 +1,11 @@
 require 'benchmark'
 
 Benchmark.bm do |bench|
-  bench.report {
+  bench.report do
     array = (1..1_000).to_a
 
     array.sort do |item, other|
       other <=> item
     end
-  }
+  end
 end
